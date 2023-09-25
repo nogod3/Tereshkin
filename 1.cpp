8c7d7358b789f287058d8a937ac4ca449ee53dee
nogod3 <146005500+nogod3@users.noreply.github.com>
2023-09-25 19:21:13 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std; 
+int main ()
+{
+    float p = 2.2;
+    float m = 6.35;
+    float k = 1.32;
+    float o = 0.40;
+    float n = 0;
+    n = 1 - 1 / p; 
+    cout << n;
+    
+    return 0;
+}
