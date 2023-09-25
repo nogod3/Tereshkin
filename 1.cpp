fd98f92bd05fa5e798d5c5eb7da325837f9ac3d4
nogod3 <146005500+nogod3@users.noreply.github.com>
2023-09-25 20:20:00 +0300

    Update 1.cpp
    Готовый код( 22 вариант)

--- 1.cpp
+++ 1.cpp
@@ -4,20 +4,20 @@
 using namespace std; 
 int main ()
 {
-    float p = 2.2;
-    float m = 6.35;
-    float k = 1.32;
-    float o = 0.40;
-    float n = 0;
-    float kpd1 = 0;
-    float kpd2 = 0;
-    float kpd3 = 0;
-    n = (k - 1) / k;
-    kpd2 = ( p - 1) * ( m * m - 1) / (pow (m , n) * ( p - 1) - o * ( p - pow(m , n)));
-    kpd3 = 1 - 1 / pow ( m , n);
-    kpd1 = 1 - 1 / p; 
-    cout << kpd1 << ' ' << kpd2 << ' ' << kpd3 ;
+  float p = 2.2;
+  float m = 6.35;
+  float k = 1.32;
+  float o = 0.40;
+  float n = 0;
+  float kpd1 = 0;
+  float kpd2 = 0;
+  float kpd3 = 0;
+  n = (k - 1) / k;
+  kpd2 = ( p - 1) * ( m * m - 1) / (pow (m , n) * ( p - 1) - o * ( p - pow(m , n)));
+  kpd3 = 1 - 1 / pow ( m , n);
+  kpd1 = 1 - 1 / p; 
+  cout << kpd1 << ' ' << kpd2 << ' ' << kpd3 ;
     
     
-    return 0;
+  return 0;
 }
