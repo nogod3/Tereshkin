68338614cd10f9ad1f44451aab28ce9835342f4f
nogod3 <146005500+nogod3@users.noreply.github.com>
2023-09-25 20:10:11 +0300

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std; 
 int main ()
 {
@@ -8,8 +9,15 @@ int main ()
     float k = 1.32;
     float o = 0.40;
     float n = 0;
-    n = 1 - 1 / p; 
-    cout << n;
+    float kpd1 = 0;
+    float kpd2 = 0;
+    float kpd3 = 0;
+    n = (k - 1) / k;
+    kpd2 = ( p - 1) * ( m * m - 1) / (pow (m , n) * ( p - 1) - o * ( p - pow(m , n)));
+    kpd3 = 1 - 1 / pow ( m , n);
+    kpd1 = 1 - 1 / p; 
+    cout << kpd1 << ' ' << kpd2 << ' ' << kpd3 ;
+    
     
     return 0;
 }
