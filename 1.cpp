c01f0d4f2a14835d58c2b02e936810bbda9d4ad9
nogod3 <146005500+nogod3@users.noreply.github.com>
2023-09-27 17:06:03 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,22 +1,37 @@
+/******************************************************************************
+Терешкин Артемий Русланович ПИ-231 
+https://onlinegdb.com/T7BT_fb5c   
+
+*******************************************************************************/
+
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+
 using namespace std; 
-int main ()
-{
-  float p = 2.2;
-  float m = 6.35;
-  float k = 1.32;
-  float o = 0.40;
-  float n = 0;
-  float kpd1 = 0;
-  float kpd2 = 0;
-  float kpd3 = 0;
+
+int main () {
+
+  double p , m , k , o , n , kpd1 , kpd2 , kpd3; 
+  
+  cout << "Введите p";
+  cin >> p;
+  cout << "Введите m";
+  cin >> m;
+  cout << "Введите k";
+  cin >> k;
+  cout << "Введите o";
+  cin >> o;
+   
   n = (k - 1) / k;
-  kpd2 = ( p - 1) * ( m * m - 1) / (pow (m , n) * ( p - 1) - o * ( p - pow(m , n)));
+  kpd1 = 1 - 1 / p;
+  kpd2 = ( p - 1) * ( m * m - 1) / (pow (m , n) * ( p - 1) - o * ( p - pow(m , n) ) );
   kpd3 = 1 - 1 / pow ( m , n);
-  kpd1 = 1 - 1 / p; 
-  cout << kpd1 << ' ' << kpd2 << ' ' << kpd3 ;
+  
+  cout << fixed << setprecision(7) << "\n" << kpd1
+                                   << "\n" << kpd2
+                                   << "\n" << kpd3;
     
     
   return 0;
