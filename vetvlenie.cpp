1ba6c1c677b348f7596db4c8721e2e5e91bc5547
nogod3 <146005500+nogod3@users.noreply.github.com>
2023-10-10 12:57:24 +0300

    vetvlenie.cpp

---
+++ vetvlenie.cpp
@@ -0,0 +1,36 @@
+/******************************************
+* Терешкин Артемий ПИ-231                 *
+* Циклы с ветвлениями, 22                 *    
+* https://onlinegdb.com/LKh-VaR17         *
+******************************************/
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    double g, v, e, shirx, r, z, K;
+    cout << "Введите v:"; 
+    cin >> v;
+    cout << "Введите e:";
+    cin >> e; 
+    cout << "Введите r:";
+    cin >> r; 
+    
+    shirx = e / r; 
+    
+    while ( (g >= 0.1) && ( g <= 5) ) {
+    K = log10( (g * e) / v ); 
+    cout << "K =" << K << '\n';
+        
+} 
+    if (K < 0.55) { 
+    z = pow ( 1 /  (0.7 + 2 * K - (2 * log10(shirx)))  , 2);
+    } else {
+    z = pow ( 1 / (1.18 + 1.13 * K - (2 * log10(e))) , 2);}
+    cout << "z =" << z << '\n';
+    
+    
+    return 0;
+}
